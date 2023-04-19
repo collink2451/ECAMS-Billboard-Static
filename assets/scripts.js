209bf06d978395dd39c7626c28dc79f6442f1a58
Collin Koldoff <collinkoldoff@gmail.com>
2023-04-19 11:38:29 -0500

    Updated ping

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,18 +1,13 @@
 const api_url = "https://ecams-billboard--api.azurewebsites.net";
+const acp_url = "https://ecams-billboard-acp.azurewebsites.net";
 
 setInterval(function () {
   ping();
 }, 120000);
 
-function ping() {
-  $.ajax({
-    type: "GET",
-    url: api_url + "/ping",
-    dataType: "text",
-    success: function (response) {
-      console.log(response);
-    },
-  });
+async function ping() {
+  await fetch(api_url + "/ping", { mode: "no-cors" });
+  await fetch(acp_url + "/ping", { mode: "no-cors" });
 }
 
 $(window).on("load", function () {
